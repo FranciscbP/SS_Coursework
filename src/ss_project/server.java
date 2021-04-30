5bb75e663d90c36af750463440478c31505e2e96
ArcVult <81184007+ArcVult@users.noreply.github.com>
2021-04-30 10:56:43 +0100

    Add files via upload

--- src/ss_project/server.java
+++ src/ss_project/server.java
@@ -2,28 +2,67 @@ package ss_project;
 import java.net.*;
 import java.io.*;  
 
+
+
 public class server 
 {        
     public static void main(String[] args) throws IOException
     {        
         ServerSocket server = new ServerSocket(9090);    
         System.out.println("Server Running...");  
-        
         while (true) 
         {
             Socket client = server.accept();
             System.out.println("Client Connected: " + client);
             
-            DataInputStream dataFromClient = new DataInputStream(client.getInputStream());                        
+            DataInputStream dataFromClient = new DataInputStream(client.getInputStream());
+            DataOutputStream dataToClient = new DataOutputStream(client.getOutputStream());
+            
             int clientType = dataFromClient.readInt();
-                                
+            
             Thread tr = new ClientHandler(client,clientType);
-            tr.start();           
+            tr.start();
+            SendData(dataFromClient, dataToClient);
         }
+    }
+public static void SendData(DataInputStream dataFromClient,DataOutputStream dataToClient){
+        while (true) { 
+            try{
+                int Temp = dataFromClient.readInt();
+                int pressure = dataFromClient.readInt();
+                int humidity = dataFromClient.readInt();
+                int windspeed = dataFromClient.readInt();
+                int winddirection = dataFromClient.readInt();
+
+                System.out.println("temperature reading is: "+Temp+ "C");
+                System.out.println("Barometric pressure reading is: 10"+pressure+"hPa");
+                System.out.println("Relative humidity reading is: "+humidity+"%");
+                System.out.println("Wind Speed is: "+windspeed+"mph");
+                System.out.println("Wind direction is: "+winddirection+"degrees");
+
+
+                dataToClient.writeInt(Temp);
+                dataToClient.flush();
+
+                dataToClient.writeInt(pressure);
+                dataToClient.flush();
+
+                dataToClient.writeInt(humidity);
+                dataToClient.flush();
+
+                dataToClient.writeInt(windspeed);
+                dataToClient.flush();
+
+                dataToClient.writeInt(winddirection);
+                dataToClient.flush();
+                
+                Thread.sleep(5 * 1000);
+            }
+        catch(Exception IOException){}
     }
 }
 
-class ClientHandler extends Thread
+static class ClientHandler extends Thread
 {
     Socket client;
     int clientType;
@@ -35,6 +74,7 @@ class ClientHandler extends Thread
 
     public void run() 
     {
+
         try
         {
             DataOutputStream dataToClient = new DataOutputStream(client.getOutputStream());
@@ -50,5 +90,6 @@ class ClientHandler extends Thread
         }
         catch(IOException e){}                    
     }
+}
 }
 
